12fae4072286aa7785246cb556fab96defc54d12
Boyke <boykeberryn@gmail.com>
2023-01-06 23:49:51 +0700

    secondary commit with create file and add image

---
+++ src/screens/SignUp.js
@@ -0,0 +1,15 @@
+import {View, Text, StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  wrapper: {},
+});
+
+const SignUp = () => {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+export default SignUp;
