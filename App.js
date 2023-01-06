0795db61c57963a116228bcbe528aea8a8c6c5a1
Boyke <boykeberryn@gmail.com>
2023-01-06 22:37:45 +0700

    first initialize

---
+++ App.js
@@ -0,0 +1,13 @@
+import {View, Text, StyleSheet} from 'react-native';
+
+const Styles = StyleSheet.create({});
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello World!</Text>
+    </View>
+  );
+};
+
+export default App;
